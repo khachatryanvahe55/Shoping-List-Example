6e9a4bb847d0043ea4db274319f92028ded67f8d
User <User@WIN-4QK4E4D33BC>
2024-02-23 18:09:50 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+string[] array = new string[] { "Tomato", "Cucumber", "Milk", "Bread", "Cheese", "Beer" };
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+while (true)
+{
+    Console.WriteLine("Enter bought item ");
+    string input = Console.ReadLine();
+    bool found = false; 
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (string.Equals(array[i], input))
+        {
+            Console.WriteLine("Item found");
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        Console.WriteLine("Item not found");
+        Console.WriteLine("Please follow in Shopping list");
+    }
+    
+}
